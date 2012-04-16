5a3d45711db5f0f0f584c435c1a24ec116a92596
Pedro Vicente <pedrovfer@gmail.com>
2012-04-16 15:24:13 +0200

    Removed: __init__.py now it is imported from PYTHONPATH

--- __init__.py
+++
@@ -1,5 +0,0 @@
-import sys, os, inspect
-library_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) # script of __init__.py directory
-
-if library_dir not in sys.path:
-    sys.path.append(library_dir)
